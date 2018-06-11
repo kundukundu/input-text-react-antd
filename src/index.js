0ed342012e5343f1365f437a692b71f827e90ce2
Chiyana Simões <csimoes@inusual.com>
2018-06-11 16:15:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Input } from 'antd';
+
+ReactDOM.render(<Input placeholder="Enter your name please" />, document.getElementById('container'));
+          
